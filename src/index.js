ea439242511b1c6ec4ac8cf51b42aeadd7aecc7c
MagicMoleMM <101579330+MagicMoleMM@users.noreply.github.com>
2022-05-28 21:50:06 +0300

    28.05.2022 21:50

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { configureStore } from '@reduxjs/toolkit'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const reducer = (state=0, action) => {
+    switch(action.type) {
+        case 'INC': 
+            return state + 1;
+        case 'DEC': 
+            return state - 1;
+        case 'RESET': 
+            return 0;
+        default:
+            return state
+    }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const store = configureStore({reducer})
+
+const inc = document.getElementById('plus');
+inc.addEventListener('click', () => {
+    store.dispatch({type: 'INC'})
+});
+
+const dec = document.getElementById('minus');
+dec.addEventListener('click', () => {
+    store.dispatch({type: 'DEC'})
+});
+
+const reset = document.getElementById('reset');
+reset.addEventListener('click', () => {
+    store.dispatch({type: 'RESET'})
+});
+
+const update = () => {
+    document.getElementById('counter').textContent = store.getState();
+}
+
+store.subscribe(update);
